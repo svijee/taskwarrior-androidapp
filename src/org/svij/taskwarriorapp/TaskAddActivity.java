57ca010b48ddb300c194b965afb35d3a9c6df0ce
Sujeevan Vijayakumaran <mail@svij.org>
2012-10-31 21:24:15 +0100

    Better Code-Layout.

--- src/org/svij/taskwarriorapp/TaskAddActivity.java
+++ src/org/svij/taskwarriorapp/TaskAddActivity.java
@@ -12,42 +12,45 @@ import android.widget.EditText;
 public class TaskAddActivity extends Activity {
 	private TaskDataSource datasource;
 
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_task_add);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
-    }
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_task_add);
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_task_add, menu);
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_task_add, menu);
+		return true;
+	}
 
-    
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                NavUtils.navigateUpFromSameTask(this);
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-    
-    public void onClick(View view) {
-    	datasource = new TaskDataSource(this);
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	public void onClick(View view) {
+		datasource = new TaskDataSource(this);
 		datasource.open();
-		
-    	EditText etTaskAdd = (EditText) findViewById(R.id.etTaskAdd);
-    	datasource.createTask(etTaskAdd.getText().toString());
-    	Intent intent = new Intent(this, MainActivity.class);
-    	startActivity(intent);
-    }
 
-    @Override
-    protected void onPause() {
-      datasource.close();
-      super.onPause();
-    }
+		EditText etTaskAdd = (EditText) findViewById(R.id.etTaskAdd);
+		datasource.createTask(etTaskAdd.getText().toString());
+		Intent intent = new Intent(this, MainActivity.class);
+		startActivity(intent);
+	}
+
+	@Override
+	protected void onPause() {
+		try {
+			datasource.close();
+		} catch (Exception e) {
+		} finally {
+			super.onPause();
+		}
+	}
 }
