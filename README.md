3720d91db9d58230318bc101b7968652fd965f41
Sujeevan Vijayakumaran <mail@svij.org>
2012-12-04 21:21:07 +0100

    Added LICENSE and README.md to the repository Added menu-drawer library which provides a left-side menu Added dummy menu-drawer.

---
+++ README.md
@@ -0,0 +1,45 @@
+Taskwarrior for Android
+=======================
+
+Taskwarrior for Android will be a comptible App for the commandline tool Taskwarrior.
+
+**See http://taskwarrior.org for more information about the CLI-Tool taskwarrior.**
+
+Developed By
+============
+
+* Sujeevan Vijayakumaran - <mail@svij.org>
+
+License
+=======
+
+Copyright (c) 2012 Sujeevan Vijayakumaran
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+Libraries
+=========
+
+Taskwarrior for Android is using following Libraries, which are licensed under
+Apache License 2.0:
+
+ * ActionBarSherlock – by Jake Wharton
+ * Android menu-drawer – by Simon Vig Therkildsen
+ 
+See LICENSE in the subdirectories com_actionbar and menu_drawer.
--- taskwarrior-androidapp/src/org/svij/taskwarriorapp/TasksActivity.java
+++ taskwarrior-androidapp/src/org/svij/taskwarriorapp/TasksActivity.java
@@ -1,17 +1,26 @@
 package org.svij.taskwarriorapp;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import net.simonvt.widget.MenuDrawer;
+import net.simonvt.widget.MenuDrawerManager;
 
 import org.svij.taskwarriorapp.data.Task;
 import org.svij.taskwarriorapp.db.TaskArrayAdapter;
 import org.svij.taskwarriorapp.db.TaskDataSource;
+import org.svij.taskwarriorapp.ui.MenuListView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.app.SherlockListFragment;
@@ -24,11 +33,20 @@ import com.actionbarsherlock.view.MenuItem;
 public class TasksActivity extends SherlockFragmentActivity {
 	TaskDataSource datasource;
 
+	private MenuDrawerManager mMenuDrawer;
+
+	private MenuAdapter mAdapter;
+	private MenuListView mList;
+
+	private int mActivePosition = -1;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		setTheme(R.style.Theme_Sherlock_Light_DarkActionBar);
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+
+		mMenuDrawer = new MenuDrawerManager(this, MenuDrawer.MENU_DRAG_WINDOW);
+		mMenuDrawer.setContentView(R.layout.activity_main);
 
 		if (getSupportFragmentManager().findFragmentById(android.R.id.content) == null) {
 			ArrayListFragment list = new ArrayListFragment();
@@ -36,11 +54,58 @@ public class TasksActivity extends SherlockFragmentActivity {
 					.add(android.R.id.content, list).commit();
 		}
 
+		// TODO: Dummy Menu for now. Implement 'real' Menu later with icons.
+		List<Object> items = new ArrayList<Object>();
+		items.add(new Category("task commands"));
+		items.add(new Item("task next"));
+		items.add(new Item("task long"));
+		items.add(new Category("Projects"));
+		items.add(new Item("project1"));
+		items.add(new Item("project2"));
+
+		// A custom ListView is needed so the drawer can be notified when it's
+		// scrolled. This is to update the position
+		// of the arrow indicator.
+		mList = new MenuListView(this);
+		mAdapter = new MenuAdapter(items);
+		mList.setAdapter(mAdapter);
+		mList.setOnItemClickListener(mItemClickListener);
+		mList.setOnScrollChangedListener(new MenuListView.OnScrollChangedListener() {
+			@Override
+			public void onScrollChanged() {
+				mMenuDrawer.getMenuDrawer().invalidate();
+			}
+		});
+
+		mMenuDrawer.setMenuView(mList);
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 	}
+
+	private AdapterView.OnItemClickListener mItemClickListener = new AdapterView.OnItemClickListener() {
+		@Override
+		public void onItemClick(AdapterView<?> parent, View view, int position,
+				long id) {
+			mActivePosition = position;
+			mMenuDrawer.setActiveView(view, position);
+			mMenuDrawer.closeMenu();
+		}
+	};
 
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getSupportMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
+	}
+
+	@Override
+	public void onBackPressed() {
+		final int drawerState = mMenuDrawer.getDrawerState();
+		if (drawerState == MenuDrawer.STATE_OPEN
+				|| drawerState == MenuDrawer.STATE_OPENING) {
+			mMenuDrawer.closeMenu();
+			return;
+		}
+
+		super.onBackPressed();
 	}
 
 	public boolean onOptionsItemSelected(MenuItem item) {
@@ -49,6 +114,9 @@ public class TasksActivity extends SherlockFragmentActivity {
 			Intent intent = new Intent(this, TaskAddActivity.class);
 			intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 			startActivity(intent);
+			return true;
+		case android.R.id.home:
+			mMenuDrawer.toggleMenu();
 			return true;
 		default:
 			return super.onOptionsItemSelected(item);
@@ -71,6 +139,25 @@ public class TasksActivity extends SherlockFragmentActivity {
 	}
 
 	public static class ArrayListFragment extends SherlockListFragment {
+
+		/**
+		 *
+		 * This class is based on GnuCash Mobiles "AccountsActivity.java" by
+		 * Ngewi Fet <ngewif@gmail.com>
+		 *
+		 * Licensed under the Apache License, Version 2.0 (the "License"); you
+		 * may not use this file except in compliance with the License. You may
+		 * obtain a copy of the License at
+		 *
+		 * http://www.apache.org/licenses/LICENSE-2.0
+		 *
+		 * Unless required by applicable law or agreed to in writing, software
+		 * distributed under the License is distributed on an "AS IS" BASIS,
+		 * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+		 * implied. See the License for the specific language governing
+		 * permissions and limitations under the License.
+		 */
+
 		TaskDataSource datasource;
 		ArrayAdapter<Task> adapter = null;
 		@SuppressWarnings("unused")
@@ -157,7 +244,8 @@ public class TasksActivity extends SherlockFragmentActivity {
 
 		public void refreshListView() {
 			ArrayList<Task> values = datasource.getAllTasks();
-			adapter = new TaskArrayAdapter(getActivity(), R.layout.task_row, values);
+			adapter = new TaskArrayAdapter(getActivity(), R.layout.task_row,
+					values);
 			setListAdapter(adapter);
 		}
 
@@ -227,4 +315,111 @@ public class TasksActivity extends SherlockFragmentActivity {
 		}
 
 	}
+
+	/*
+	 * Item, which represents an item in the MenuList
+	 */
+
+	private static class Item {
+
+		String mTitle;
+
+		Item(String title) {
+			mTitle = title;
+		}
+	}
+
+	/*
+	 * Category, which represents a category in the MenuList
+	 */
+
+	private static class Category {
+
+		String mTitle;
+
+		Category(String title) {
+			mTitle = title;
+		}
+	}
+
+	/*
+	 * The MenuAdapter
+	 */
+	public class MenuAdapter extends BaseAdapter {
+
+		private List<Object> mItems;
+
+		MenuAdapter(List<Object> items) {
+			mItems = items;
+		}
+
+		@Override
+		public int getCount() {
+			return mItems.size();
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return mItems.get(position);
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return position;
+		}
+
+		@Override
+		public int getItemViewType(int position) {
+			return getItem(position) instanceof Item ? 0 : 1;
+		}
+
+		@Override
+		public int getViewTypeCount() {
+			return 2;
+		}
+
+		@Override
+		public boolean isEnabled(int position) {
+			return getItem(position) instanceof Item;
+		}
+
+		@Override
+		public boolean areAllItemsEnabled() {
+			return false;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View v = convertView;
+			Object item = getItem(position);
+
+			if (item instanceof Category) {
+				if (v == null) {
+					v = getLayoutInflater().inflate(R.layout.menu_row_category,
+							parent, false);
+				}
+
+				((TextView) v).setText(((Category) item).mTitle);
+
+			} else {
+				if (v == null) {
+					v = getLayoutInflater().inflate(R.layout.menu_row_item,
+							parent, false);
+				}
+
+				TextView tv = (TextView) v;
+				tv.setText(((Item) item).mTitle);
+			}
+
+			v.setTag(R.id.mdActiveViewPosition, position);
+
+			if (position == mActivePosition) {
+				mMenuDrawer.setActiveView(v, position);
+			}
+
+			return v;
+		}
+
+	}
+
 }
