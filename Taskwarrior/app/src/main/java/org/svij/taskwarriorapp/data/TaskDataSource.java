05f55b0e3e2c072ff3750057eeb04af1e5bad6f9
Sujeevan Vijayakumaran <mail@svij.org>
2015-04-19 20:16:48 +0200

    TaskDataSource: implemented inserTask, getAllTasks and getTask

--- Taskwarrior/app/src/main/java/org/svij/taskwarriorapp/data/TaskDataSource.java
+++ Taskwarrior/app/src/main/java/org/svij/taskwarriorapp/data/TaskDataSource.java
@@ -1,9 +1,16 @@
 package org.svij.taskwarriorapp.data;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import org.svij.taskwarriorapp.Task;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
 
 public class TaskDataSource {
 
@@ -11,7 +18,11 @@ public class TaskDataSource {
     private TaskDbHelper dbHelper;
 
     private String[] columns = {
-
+            TaskDbHelper.COLUMN_ID,
+            TaskDbHelper.COLUMN_UUID,
+            TaskDbHelper.COLUMN_ENTRY,
+            TaskDbHelper.COLUMN_STATUS,
+            TaskDbHelper.COLUMN_DESCRIPTION
     };
 
     public TaskDataSource(Context context) {
@@ -24,5 +35,46 @@ public class TaskDataSource {
 
     public void close() {
         dbHelper.close();
+    }
+
+    public void insertTask(Task task) {
+        ContentValues values = new ContentValues();
+        values.put(TaskDbHelper.COLUMN_UUID, task.getUuid().toString());
+        values.put(TaskDbHelper.COLUMN_ENTRY, task.getEntry().getTime());
+        values.put(TaskDbHelper.COLUMN_STATUS, task.getStatus());
+        values.put(TaskDbHelper.COLUMN_DESCRIPTION, task.getDescription());
+
+        long insertId = database.insert(TaskDbHelper.TABLE_TASK, null, values);
+    }
+
+    public List<Task> getAllTasks() {
+        List<Task> taskList = new ArrayList<Task>();
+
+        Cursor cursor = database.query(TaskDbHelper.TABLE_TASK, columns, null, null, null, null, null);
+        cursor.moveToFirst();
+
+        while (!cursor.isAfterLast()) {
+            Task task = getTask(cursor);
+            taskList.add(task);
+            cursor.moveToNext();
+        }
+
+        cursor.close();
+        return taskList;
+    }
+
+    public Task getTask(Cursor cursor) {
+        int uuidIndex = cursor.getColumnIndex(TaskDbHelper.COLUMN_UUID);
+        int entryIndex = cursor.getColumnIndex(TaskDbHelper.COLUMN_ENTRY);
+        int statusIndex = cursor.getColumnIndex(TaskDbHelper.COLUMN_STATUS);
+        int descriptionIndex = cursor.getColumnIndex(TaskDbHelper.COLUMN_DESCRIPTION);
+
+        Task task = new Task();
+        task.setUUID(UUID.fromString(cursor.getString(uuidIndex)));
+        task.setEntry(new Date(cursor.getLong(entryIndex)));
+        task.setStatus(cursor.getString(statusIndex));
+        task.setDescription(cursor.getString(descriptionIndex));
+
+        return task;
     }
 }
